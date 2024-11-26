365dba0ab19bc047a578d7659ba6b58f8f2c9af8
Aryan Singh <108578411+Aryan0116@users.noreply.github.com>
2024-11-27 01:44:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ function displayForecast(data) {
 
 async function fetchAirQualityData(lat, lon) {
     try {
-        const aqiUrl = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+        const aqiUrl = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
         
         const response = await fetch(aqiUrl);
         const data = await response.json();
